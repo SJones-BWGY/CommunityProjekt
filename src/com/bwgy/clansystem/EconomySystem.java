6321529da51f722bf76b9eb35dc9506931aa42f6
S_Jones <jones.bwgy@gmail.com>
2020-06-18 20:10:00 +0200

    Create EconomySystem.java

---
+++ src/com/bwgy/clansystem/EconomySystem.java
@@ -0,0 +1,137 @@
+package com.bwgy.clansystem;
+
+import com.bwgy.main.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Wither;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class EconomySystem implements Listener {
+    public static ArrayList indebted_players=new ArrayList<UUID>();
+    private static FileConfiguration config;
+    private static File configfile;
+    @EventHandler
+    public void onBreak(BlockBreakEvent e){
+        addMoney(e.getPlayer(),1);
+    }
+    @EventHandler
+    public void onPlace(BlockPlaceEvent e){
+        if(!(getMoney(e.getPlayer())<=0)){
+            removeMoney(e.getPlayer(),1);
+        }
+
+    }
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e){
+        if(e.getEntity().getKiller() != null){
+            if(e.getEntity() instanceof EnderDragon){
+                Config.addPoints(Config.getClan(e.getEntity().getKiller()),1000);
+            }else if(e.getEntity() instanceof Wither){
+                Config.addPoints(Config.getClan(e.getEntity().getKiller()),1000);
+            }else{
+                Config.addPoints(Config.getClan(e.getEntity().getKiller()),1);
+            }
+        }
+    }
+    public static FileConfiguration getConfig(){
+        if (configfile == null) {
+            configfile = new File(Main.getPlugin().getDataFolder(), "money.yml");
+        }
+        config = YamlConfiguration.loadConfiguration(configfile);
+
+        // Look for defaults in the jar
+        Reader defConfigStream = null;
+        try {
+            defConfigStream = new InputStreamReader(Main.getPlugin().getResource("money.yml"), "UTF8");
+        } catch (UnsupportedEncodingException e) {
+            Main.getPlugin().getLogger().severe("Something wen't wrong (X_X)");
+            Main.getPlugin().getLogger().severe(e.getMessage());
+            Main.getPlugin().getServer().getPluginManager().disablePlugin(Main.getPlugin());
+        }
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            config.setDefaults(defConfig);
+        }
+        return config;
+    }
+    public static int getMoney(@NotNull Player p){
+        return getConfig().getInt(String.valueOf(p.getUniqueId()));
+    }
+    public static void addMoney(@NotNull Player p, @NotNull Integer value){
+        FileConfiguration tmp=getConfig();
+        tmp.set(String.valueOf(p.getUniqueId()), value+tmp.getInt(String.valueOf(p.getUniqueId())));
+        try {
+            tmp.save(configfile);
+        } catch (IOException e) {
+            Main.getPlugin().getLogger().severe("Something wen't wrong (X_X)");
+            Main.getPlugin().getLogger().severe(e.getMessage());
+            Main.getPlugin().getServer().getPluginManager().disablePlugin(Main.getPlugin());
+        }
+    }
+    public static void setMoney(@NotNull Player p, @NotNull Integer value){
+        FileConfiguration tmp=getConfig();
+        tmp.set(String.valueOf(p.getUniqueId()), value);
+        try {
+            tmp.save(configfile);
+        } catch (IOException e) {
+            Main.getPlugin().getLogger().severe("Something wen't wrong (X_X)");
+            Main.getPlugin().getLogger().severe(e.getMessage());
+            Main.getPlugin().getServer().getPluginManager().disablePlugin(Main.getPlugin());
+        }
+    }
+    public static void checkPlayer(@NotNull Player p) {
+        Main.getPlugin().getLogger().info("Checking "+p.getName()+"...");
+        if (getMoney(p) < 0) {
+            Main.getPlugin().getLogger().info("Player has less money than 0");
+                if(!indebted_players.contains(p.getUniqueId())) {
+                    Main.getPlugin().getLogger().info("Player is indebted!");
+                    p.sendMessage("§4Du bist verschuldet! Farme §c" + Math.abs(getMoney(p)) + "§4 Blöcke!");
+                    indebted_players.add(p.getUniqueId());
+                    Main.getPlugin().getLogger().info("Making the Task ready...");
+                    Bukkit.getScheduler().runTaskLater(Main.getPlugin(), new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if (getMoney(p) < 0) {
+                                if (Config.isLeader(Config.getClan(p), p)) {
+                                    p.sendMessage("§4Du bist zu lange verschuldet! Dein Clan wird gelöscht!");
+                                } else {
+                                    p.sendMessage("§4Du bist zu lange verschuldet! Du wirst aus deinem Clan gekickt!");
+                                }
+                                Config.kickPlayer(Config.getClan(p), Bukkit.getOfflinePlayer(p.getUniqueId()));
+                            }
+                            indebted_players.remove(p.getUniqueId());
+
+                        }
+                    }, 6000);
+                    Main.getPlugin().getLogger().info("Task ready!");
+                }
+        }
+        return;
+    }
+    public static void removeMoney(@NotNull Player p, @NotNull Integer value){
+        FileConfiguration tmp=getConfig();
+        Integer playermoney=(getConfig().getInt(String.valueOf(p.getUniqueId())));
+        tmp.set(String.valueOf(p.getUniqueId()), playermoney-value);
+        try {
+            tmp.save(configfile);
+        } catch (IOException e) {
+            Main.getPlugin().getLogger().severe("Something wen't wrong (X_X)");
+            Main.getPlugin().getLogger().severe(e.getMessage());
+            Main.getPlugin().getServer().getPluginManager().disablePlugin(Main.getPlugin());
+        }
+    }
+
+}
