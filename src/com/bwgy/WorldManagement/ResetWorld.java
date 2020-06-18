6d5e3434cfd9414fea4efd9e609a1b9df1f49eab
S_Jones <jones.bwgy@gmail.com>
2020-06-18 20:09:14 +0200

    Create ResetWorld.java

---
+++ src/com/bwgy/WorldManagement/ResetWorld.java
@@ -0,0 +1,15 @@
+package com.bwgy.WorldManagement;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+
+public class ResetWorld implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
